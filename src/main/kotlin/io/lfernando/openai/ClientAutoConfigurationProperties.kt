a096c802a8e17427159eacebb421f675f5314c3f
Luis Hernandez <lfernando2091@gmail.com>
2023-07-27 00:36:18 -0600

    removed pom

--- src/main/kotlin/io/lfernando/openai/ClientAutoConfigurationProperties.kt
+++ src/main/kotlin/io/lfernando/openai/ClientAutoConfigurationProperties.kt
@@ -3,11 +3,11 @@ package io.lfernando.openai
 import org.springframework.boot.context.properties.ConfigurationProperties
 
 @ConfigurationProperties(prefix = "openai")
-class ClientAutoConfigurationProperties(
+data class ClientAutoConfigurationProperties(
     /**
      * OpenAI Api Key
      */
-    val apiKey: String,
-    val baseUrl: String = "https://api.openai.com/v1",
-    val organizationId: String? = null,
+    var apiKey: String = "",
+    var baseUrl: String = "https://api.openai.com/v1",
+    var organizationId: String? = null,
 )
